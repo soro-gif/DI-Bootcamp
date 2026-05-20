9d39e6289a5478645f8ecc6fccfb06e80e09d684
LAMOUSSA SORO <lamoussa.soro@uvci.edu.ci>
2026-05-20 09:13:30 +0000

    correction

--- week1/Day1/DailyChalenge/ExerciceXP/dailychallenge.py
+++ week1/Day1/DailyChalenge/ExerciceXP/dailychallenge.py
@@ -1,34 +1,54 @@
-# =========================================================
+
 # Challenge 1 : List of Multiples
-# =========================================================
 
-# Ask the user for a number
-number = int(input("Enter a number: "))
+def generate_multiples():
+    """
+    Ask the user for a number and a length.
+    Create and display a list of multiples of the number.
+    """
+    try:
+        # Ask the user for a number
+        number = int(input("Enter a number: "))
+        
+        # Ask the user for the desired length
+        length = int(input("Enter the length of the list: "))
+        
+        # Create a list of multiples
+        multiples = [number * i for i in range(1, length + 1)]
+        
+        # Display the final list
+        print("List of multiples:", multiples)
+    except ValueError:
+        print("Invalid input. Please enter whole numbers only.")
 
-# Ask the user for the desired length
-length = int(input("Enter the length of the list: "))
 
-# Create an empty list
-multiples = []
 
-# Loop to generate multiples
-for i in range(1, length + 1):
+# Challenge 2 : Remove Consecutive Duplicate Letters
 
-    # Add multiples to the list
-    multiples.append(number * i)
 
-# Display the final list
-print("List of multiples:", multiples)
+def remove_consecutive_duplicates():
+    """
+    Ask the user for a word and remove consecutive duplicate letters.
+    """
+    word = input("Enter a word: ")
+    
+    if not word:
+        print("")
+    else:
+        # Use a list for efficient string building
+        char_list = [word[0]]
+        for letter in word[1:]:
+            if letter != char_list[-1]:
+                char_list.append(letter)
+        print(''.join(char_list))
 
-#Challenge 2 : Remove Consecutive Duplicate Letters
-word = input("Enter a word: ")
-if not word:
-    print("")
-else:
-    result = word[0]
-    for letter in word[1:]:
-        if letter != result[-1]:
-            result += letter
-    print(result)
+
+# Main Execution
+
+
+if __name__ == "__main__":
+    generate_multiples()
+    print('\n' + '-'*20 + '\n')
+    remove_consecutive_duplicates()
 
 # 
